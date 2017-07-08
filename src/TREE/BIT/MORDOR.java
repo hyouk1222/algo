5f525ca65c0c371ed08de335b7c80ce26ece9306
Hyouk Hwang <hyouk1222@gmail.com>
2017-07-08 12:55:43 +0900

    -

--- src/TREE/BIT/MORDOR.java
+++ src/TREE/BIT/MORDOR.java
@@ -1,10 +1,30 @@
 package TREE.BIT;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+
 public class MORDOR {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	static BufferedReader br;
+	static BufferedWriter bw;
+	static StringBuilder sb;
+	
+	public static void main(String[] args) throws Exception {
+		System.setIn(new FileInputStream(new File("./input.txt")));
+		br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		sb = new StringBuilder();
+		
+		
+		
+		br.close();
+//		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
 	}
 
 }
